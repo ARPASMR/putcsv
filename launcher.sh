9c78c1381ee492538580c4f740d8b0abf54f7cac
mariaranci <31917865+mariaranci@users.noreply.github.com>
2020-04-01 16:30:35 +0200

    Update launcher.sh
    tolto mount a cartella di macchina windows perchè il trasferimento è via ftp

--- launcher.sh
+++ launcher.sh
@@ -5,17 +5,11 @@
 
 numsec=600 # 10 minuti 
 
-# mount della cartella windows del REM
-mount -t cifs -o username=$USER_REM,password=$PASSWORD_REM "$SHARE_REM" /mnt
-
 while [ 1 ]
 do
   if [ $SECONDS -ge $numsec ]
   then
     SECONDS=0
-      #eseguo lo script ma solo se la cartella è montata
-      if mountpoint -q /mnt 
-      then
          ./putcsv.sh
          STATO=$?
          echo "STATO USCITA SCRIPT ====> "$STATO
@@ -24,15 +18,5 @@ do
            exit 1
          fi
          sleep $numsec
-      else 
-         echo "share windows non correttamente montata, rieseguo mount"
-         mount -t cifs -o username=$USER_REM,password=$PASSWORD_REM "$SHARE_REM" /mnt
-         STATO=$?
-         echo "STATO USCITA MOUNT ====> "$STATO
-         if [ "$STATO" -gt 0 ] # se si sono verificate anomalie esci 
-         then
-           exit 1
-         fi
-      fi
   fi
 done
